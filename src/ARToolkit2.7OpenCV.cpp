64da1ba40b5d39bb158b51cfd5cd662683128565
Daniel Bentall <dbentall@gmail.com>
2011-09-29 16:21:11 +1300

    Changed detection to use arDetectMarkerConf instead

--- src/ARToolkit2.7OpenCV.cpp
+++ src/ARToolkit2.7OpenCV.cpp
@@ -72,7 +72,7 @@ std::vector<MarkerTransform> RegistrationARToolkit::performRegistration(IplImage
 	cvCvtColor(frame_input, bgraImage, CV_RGB2BGRA);
 
 	//Find Markers
-	arDetectMarker((ARUint8 *)bgraImage->imageData, 100, &marker_info, &marker_num);
+	arDetectMarkerConf((ARUint8 *)bgraImage->imageData, 100, &marker_info, &marker_num, 0.1);
 
 	//For all Markers
 	for (int i=0; i<marker_num; i++) {
